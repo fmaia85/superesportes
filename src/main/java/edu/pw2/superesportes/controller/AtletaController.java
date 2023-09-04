9ef000b6e8eaeecd6f549b52687e90b39f0a862f
Maia <felipe.maia@ifce.edu.br>
2023-09-04 10:32:27 -0300

    Restaurando Controller de Atleta.

--- src/main/java/edu/pw2/superesportes/controller/AtletaController.java
+++ src/main/java/edu/pw2/superesportes/controller/AtletaController.java
@@ -10,41 +10,5 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/atletas")
 public class AtletaController {
 
-    // Exemplo de tratamento de GET simples.
-    // Na URL, testar com: 
-    // http://localhost:8080/atletas
-    @GetMapping()
-    @ResponseBody
-    public String olaMundo(){
-        String resposta = "<h1>Olá mundo</h1>";
-        return resposta;
-    }
-
-
-    // Exemplo de tratamento de GET com parâmetros.
-    // Na URL, testar com: 
-    // http://localhost:8080/atletas/imc?nome=Joao&altura=1.85&peso=85
-    @GetMapping("/imc")
-    @ResponseBody
-    public String calcIMC(String nome, int peso, double altura){
-        double imc = peso/(altura*altura);
-        String resposta = nome + " possui IMC: " + imc + ".";
-        return resposta;
-    }
-
-    // Exemplo de tratamento de Post com parâmetros.
-    // Na URL, testar utilizando extensão Restman com:
-    // URL: http://localhost:8080/atletas/imc
-    // Body:
-    // nome: Joao
-    // altura: 1.85
-    // peso: 85
-    @PostMapping("/imc")
-    @ResponseBody
-    public String calcIMCPost(String nome, int peso, double altura){
-        double imc = peso/(altura*altura);
-        String resposta = nome + " possui IMC: " + imc + ".";
-        return resposta;
-    }
-
+  
 }
