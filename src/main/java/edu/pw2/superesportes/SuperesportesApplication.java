e71e960ec26e419c8d55c0e9a6d8205cc71f2b81
Maia <felipe.maia@ifce.edu.br>
2023-09-04 09:46:43 -0300

    Inicialização do projeto.

---
+++ src/main/java/edu/pw2/superesportes/SuperesportesApplication.java
@@ -0,0 +1,13 @@
+package edu.pw2.superesportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperesportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperesportesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pw2/superesportes/SuperesportesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pw2.superesportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperesportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
