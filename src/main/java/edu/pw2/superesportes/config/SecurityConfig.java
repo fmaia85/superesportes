e3146bff7a323f43c071dc961502d929ae83ad0a
Felipe <felipe.ja.maia@gmail.com>
2023-09-19 09:27:38 -0300

    Carregando tela de login custom e executando logout.

--- src/main/java/edu/pw2/superesportes/config/SecurityConfig.java
+++ src/main/java/edu/pw2/superesportes/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package edu.pw2.superesportes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -27,5 +29,18 @@ public class SecurityConfig {
                 .roles("USER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(admin);
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                (authorize) -> authorize.anyRequest().authenticated()
+        ).formLogin(
+                form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/atletas")
+                        .permitAll()
+        );
+        return http.build();
     }
 }
---
+++ src/main/java/edu/pw2/superesportes/controller/LoginController.java
@@ -0,0 +1,30 @@
+package edu.pw2.superesportes.controller;
+
+import edu.pw2.superesportes.model.atleta.Atleta;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+    SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+
+
+    @GetMapping("/login")
+    public String loadLoginForm(){
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String performLogout(Authentication authentication, HttpServletRequest request, HttpServletResponse response) {
+        this.logoutHandler.logout(request, response, authentication);
+        return "redirect:/";
+    }
+}
